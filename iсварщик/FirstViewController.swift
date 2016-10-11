1a30052b57bc42c789cbbc923d331afae28610f0
Сергей Копытов <itunes.2011@mail.ru>
2016-10-11 18:42:25 +0300

    Initial Commit

---
+++ iсварщик/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iсварщик
+//
+//  Created by Sergey Kopytov on 11.10.16.
+//  Copyright © 2016 Sergey Kopytov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
