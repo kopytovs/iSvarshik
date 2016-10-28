1f8fa68e9677bc91ddb8177d1b36147914f4ff71
kopytovs <kopytov@me.com>
2016-10-28 03:52:02 +0300

    info.0.8b

---
+++ iсварщик/ThirdViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ThirdViewController.swift
+//  iсварщик
+//
+//  Created by Sergey Kopytov on 28.10.16.
+//  Copyright © 2016 Sergey Kopytov. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class ThirdViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func Call(_ sender: Any) {
+        if let url = URL(string: "tel://89990277233") {
+            if #available(iOS 10, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: {(success) in print(success)
+                })
+            } else {
+                let success = UIApplication.shared.openURL(url)
+                print(success)
+            }
+        }
+    }
+    
+    @IBAction func send(_ sender: Any) {
+        sendEmail()
+    }
+    
+    func sendEmail() {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["kopytov@me.com"])
+            mail.setMessageBody("<p>Здраствуйте, Сергей!</p>", isHTML: true)
+            
+            present(mail, animated: true)
+        } else {
+            // show failure alert
+        }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
+    }
+    
+    
+}
