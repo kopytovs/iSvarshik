56504233594950e413ff5c7da5a38cf9f53d5446
kopytovs <kopytov@me.com>
2016-11-09 01:37:28 +0300

    0.9b
    + added Video Lessons

---
+++ iсварщик/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  iсварщик
+//
+//  Created by Sergey Kopytov on 09.11.16.
+//  Copyright © 2016 Sergey Kopytov. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var NameOfTheVideo: UILabel!
+    @IBOutlet weak var VideoL: UIWebView!
+    
+}
